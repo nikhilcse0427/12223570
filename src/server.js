286c49b2ef0a8e6e64c09dd2eb377708e7e1b39e
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-07-14 13:10:55 +0530

    db connected successfully

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' });
+dotenv.config();
 import connectDB from './db/db.js';
 
 import app from './app.js'
 
 const port = process.env.PORT || 3000
-console.log(process.env.PORT)
-console.log(process.env.MONGODB_URI)
+
 connectDB()
   .then(() => {
     app.listen(port, () => {
@@ -15,6 +14,6 @@ connectDB()
   })
   .catch((error) => {
     console.error('Database connection failed:', error);
-    process.exit(1); // Optional: exit the process if DB connection fails
+    process.exit(1); 
   });
 
--- src/services/user.services.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-
-export createUser = aync({email, userName, mobileNo, accessCode, rollNo})=>{
-  
-}
